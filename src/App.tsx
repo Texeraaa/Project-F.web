40da38451ece903fc270afad168a82468da77256
Pedro Teixeira <pedro.teixeira@gruponsc.com.br>
2024-04-12 00:45:11 -0300

    configurando eslint e prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
 
 export function App() {
   return (
     <>
       <Button>Click Me</Button>
     </>
-  );
+  )
 }
-
