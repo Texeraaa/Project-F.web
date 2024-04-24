18a9ea58362eb10e7674ca502824727ebf4149af
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-04-24 11:30:49 -0300

    feat: adding first card on screen

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,10 +1,27 @@
 import { Helmet } from 'react-helmet-async'
 
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
 export function Dashboard() {
   return (
-    <>
+    <section className="flex gap-3 p-3">
       <Helmet title="Dashboard" />
-      <h1>Dashboard</h1>
-    </>
+
+      <Card className="min-w-96">
+        <CardHeader>
+          <CardTitle>Saldo total</CardTitle>
+          <CardDescription>Seu saldo total da carteira digital</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <p className="text-2xl font-bold">R$12,90</p>
+        </CardContent>
+      </Card>
+    </section>
   )
 }
