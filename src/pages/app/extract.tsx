5ca198e09a36cb75a3e0ab79f3e1b616fef690c1
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-04-26 08:22:24 -0300

    feat(pages):`extract`: creating extract page

---
+++ src/pages/app/extract.tsx
@@ -0,0 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
+export function Extracts() {
+  return (
+    <section className="flex h-full items-center justify-center gap-3  p-3 antialiased">
+      <Helmet title="Extratos" />
+      <h1 className="text-3xl font-black">Coming soon</h1>
+    </section>
+  )
+}
