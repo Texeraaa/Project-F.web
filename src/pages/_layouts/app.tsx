d57b3b1b5a81727b9dac4dd40fef0ce487bf0333
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-04-26 08:04:41 -0300

    feat(_layouts):`app`: modifing app layout

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -4,9 +4,11 @@ import { Header } from '@/components/header'
 
 export function AppLayout() {
   return (
-    <div>
-      <Header />
-      <div>
+    <div className="grid min-h-screen grid-cols-10">
+      <div className="col-span-2">
+        <Header />
+      </div>
+      <div className="">
         <Outlet />
       </div>
     </div>
