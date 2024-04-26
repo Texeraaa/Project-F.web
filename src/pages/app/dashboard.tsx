2a8fca04982a90a60f5b07c417cfab81b3c5f1dc
Pedro Teixeira <pedro.henrique.ph2018@hotmail.com>
2024-04-26 08:22:02 -0300

    feat(pages):`dashboard`: adding firsts cards

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,4 +1,6 @@
+import { ChevronRight } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
+import { Link } from 'react-router-dom'
 
 import {
   Card,
@@ -7,21 +9,46 @@ import {
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-
 export function Dashboard() {
   return (
-    <section className="flex gap-3 p-3">
+    <section className="flex h-full flex-col gap-3  p-3 antialiased">
       <Helmet title="Dashboard" />
-
-      <Card className="min-w-96">
-        <CardHeader>
-          <CardTitle>Saldo total</CardTitle>
-          <CardDescription>Seu saldo total da carteira digital</CardDescription>
-        </CardHeader>
-        <CardContent>
-          <p className="text-2xl font-bold">R$12,90</p>
-        </CardContent>
-      </Card>
+      <div className="flex items-start justify-center gap-3">
+        <Card className="w-1/2 border-primary">
+          <CardHeader>
+            <CardTitle>Saldo total</CardTitle>
+            <CardDescription>
+              Seu saldo total da carteira digital
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-between ">
+            <p className="text-2xl font-bold">R$12,90</p>
+            <Link
+              to={'/extracts'}
+              className="flex cursor-pointer items-center border-b-2 border-primary text-xl font-normal"
+            >
+              <ChevronRight />
+              Ver extrato
+            </Link>
+          </CardContent>
+        </Card>{' '}
+        <Card className="w-1/2 border-primary">
+          <CardHeader>
+            <CardTitle>Total gasto no mês</CardTitle>
+            <CardDescription>Seu total gasto no mês</CardDescription>
+          </CardHeader>
+          <CardContent className="flex justify-between ">
+            <p className="text-2xl font-bold">R$1200,90</p>
+            <Link
+              to={'/extracts'}
+              className="flex cursor-pointer items-center border-b-2 border-primary text-xl font-normal"
+            >
+              <ChevronRight />
+              Ver extrato
+            </Link>
+          </CardContent>
+        </Card>
+      </div>
     </section>
   )
 }
