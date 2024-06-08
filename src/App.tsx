ff7fe1e9f3c62cc43d3a7f3661a1836a48aadd53
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2024-06-08 12:58:39 -0300

    fixing routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,19 @@ import '../app/globals.css'
 
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
 
 import { ThemeProvider } from './components/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <HelmetProvider>
         <Helmet titleTemplate="%s | Finance" />
+        <Toaster richColors />
         <RouterProvider router={router} />
-      </ThemeProvider>
-    </HelmetProvider>
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,20 +5,14 @@ import { SignIn } from '@/pages/auth/sign-in'
 
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
-import { Extracts } from './pages/app/extract'
 
 export const router = createBrowserRouter([
   {
-    path: '/',
     element: <AppLayout />,
-    children: [
-      { path: '/', element: <Dashboard /> },
-      { path: 'extracts', element: <Extracts /> },
-    ],
+    children: [{ path: '/', element: <Dashboard /> }],
   },
   {
-    path: '/auth',
     element: <AuthLayout />,
-    children: [{ path: 'sign-in', element: <SignIn /> }],
+    children: [{ path: '/sign-in', element: <SignIn /> }],
   },
 ])
