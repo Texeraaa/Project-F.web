f3243a567d2432de68baa544e4e23fe7743513a6
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2024-06-08 13:22:34 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import path from "node:path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import path from 'node:path'
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
