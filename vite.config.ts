cccd615e4f3e340390f1e63a228fc24934512a1c
Texeraaa <pedro.henrique.ph2018@hotmail.com>
2024-06-08 13:24:14 -0300

    aaaaa

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,6 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
